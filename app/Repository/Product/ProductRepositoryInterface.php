47ca6fe1cb245c961d0b8093922680a98bc97e34
bruno-dev9900 <mafobruno990@gmail.com>
2021-12-12 17:28:35 +0100

    module produit complet

--- app/Repository/Product/ProductRepositoryInterface.php
+++ app/Repository/Product/ProductRepositoryInterface.php
@@ -5,6 +5,10 @@ namespace App\Repository\Product;
 interface ProductRepositoryInterface
 {
     public function create(array $data);
+
     public function update(array $data);
+
     public function getById(int $id);
+
+    public function delete(int $id);
 }
